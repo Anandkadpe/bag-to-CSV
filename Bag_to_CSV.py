1ba7a2b9f3cdfe6cbd316477625579f7c67a375d
unknown <anandkadpe2@gmail.com>
2024-06-04 17:21:06 +0200

    adding the skip topic

--- Bag_to_CSV.py
+++ Bag_to_CSV.py
@@ -1,4 +1,3 @@
-#import all the required python libraries
 import os
 import csv
 import rosbag
@@ -6,72 +5,74 @@ import multiprocessing
 import time
 import psutil
 
+# Define topics to skip
+TOPICS_TO_SKIP = ['/camera/color/camera_info', '/camera/color/image_raw','/camera/depth/color/points','/camera/depth/image_rect_raw']  # Add the topics you want to skip
+
 def find_rosbag_files(raw_data_folder_path):
     """
     Find all rosbag files in a folder and its subfolders
     """
-    for root, dirs, files in os.walk(raw_data_folder_path):  # Recursively walk through the folder structure
-        for file in files:  # Iterate through files in the current directory
-            if file.endswith(".bag"):  # Check if the file has a .bag extension
-                yield os.path.join(root, file)  # Yield the full path of the bag file
+    for root, dirs, files in os.walk(raw_data_folder_path):
+        for file in files:
+            if file.endswith(".bag"):
+                yield os.path.join(root, file)
 
 def extract_topics(rosbag_file):
     """
     Extract topics from a ROS bag file
     """
     try:
-        bag = rosbag.Bag(rosbag_file)  # Open the ROS bag file
-        for topic, msg, t in bag.read_messages():  # Iterate through messages in the bag file
-            yield topic, t.to_sec(), msg  # Yield topic, timestamp, and message
-        bag.close()  # Close the bag file
-    except rosbag.ROSBagException as e:  # Catch exceptions related to ROS bag processing
-        print(f"Error processing {rosbag_file}: {str(e)}")  # Print an error message if an exception occurs
-
+        bag = rosbag.Bag(rosbag_file)
+        for topic, msg, t in bag.read_messages():
+            if topic not in TOPICS_TO_SKIP:  # Skip topics in the skip list
+                yield topic, t.to_sec(), msg
+        bag.close()
+    except rosbag.ROSBagException as e:
+        print(f"Error processing {rosbag_file}: {str(e)}")
 
 def extract_message_attributes(message, parent_field=None):
     """
     Extract attributes from the message and return as a dictionary
     """
-    attributes = {}  # Initialize an empty dictionary to store attributes
-    for field in message.__slots__:  # Iterate through slots of the message
-        full_field_name = f"{parent_field}.{field}" if parent_field else field  # Construct the full field name
-        if hasattr(getattr(message, field), '__slots__'):  # Check if the field is a nested message
-            attributes.update(extract_message_attributes(getattr(message, field), parent_field=full_field_name))  # Recursively extract attributes
+    attributes = {}
+    for field in message.__slots__:
+        full_field_name = f"{parent_field}.{field}" if parent_field else field
+        if hasattr(getattr(message, field), '__slots__'):
+            attributes.update(extract_message_attributes(getattr(message, field), parent_field=full_field_name))
         else:
-            attributes[full_field_name] = getattr(message, field)  # Extract attribute value
-    return attributes  # Return the dictionary of attributes
+            attributes[full_field_name] = getattr(message, field)
+    return attributes
 
 def convert_to_csv(topic_generator, output_folder):
     """
     Convert topics from topic_generator to CSV files
     """
-    for topic, timestamp, message in topic_generator:  # Iterate through topics, timestamps, and messages
+    for topic, timestamp, message in topic_generator:
         try:
-            csv_file_path = os.path.join(output_folder, f"{topic.replace('/', '_')}.csv")  # Generate the CSV file path
-            with open(csv_file_path, 'a', newline='') as csv_file:  # Open the CSV file in append mode
-                writer = csv.DictWriter(csv_file, fieldnames=['Timestamp'] + list(extract_message_attributes(message).keys()))  # Create a CSV writer with headers
-                if csv_file.tell() == 0:  # Check if the file is empty
-                    writer.writeheader()  # Write header if the file is empty
-                attributes = extract_message_attributes(message)  # Extract message attributes
-                attributes['Timestamp'] = timestamp  # Add timestamp to attributes
-                writer.writerow(attributes)  # Write attributes to CSV
+            csv_file_path = os.path.join(output_folder, f"{topic.replace('/', '_')}.csv")
+            with open(csv_file_path, 'a', newline='') as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=['Timestamp'] + list(extract_message_attributes(message).keys()))
+                if csv_file.tell() == 0:
+                    writer.writeheader()
+                attributes = extract_message_attributes(message)
+                attributes['Timestamp'] = timestamp
+                writer.writerow(attributes)
         except Exception as e:
             print(f"Error processing {topic}: {e}. Skipping this topic.")
-
 
 def process_bag_file(bag_file):
     """
     Process a single bag file
     """
     try:
-        start_time = time.time()  # Record the start time
-        output_folder = os.path.splitext(bag_file)[0] + "_csv"  # Generate the output folder path
-        os.makedirs(output_folder, exist_ok=True)  # Create the output folder if it doesn't exist
-        topic_generator = extract_topics(bag_file)  # Generate topics from the bag file
-        convert_to_csv(topic_generator, output_folder)  # Convert topics to CSV files
-        end_time = time.time()  # Record the end time
-        elapsed_time = end_time - start_time  # Calculate the elapsed time
-        print(f"Processed {bag_file} in {elapsed_time:.2f} seconds")  # Print the elapsed time
+        start_time = time.time()
+        output_folder = os.path.splitext(bag_file)[0] + "_csv"
+        os.makedirs(output_folder, exist_ok=True)
+        topic_generator = extract_topics(bag_file)
+        convert_to_csv(topic_generator, output_folder)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Processed {bag_file} in {elapsed_time:.2f} seconds")
     except OSError as e:
         print(f"Error processing {bag_file}: {e}")
 
@@ -79,16 +80,15 @@ def process_bag_files(raw_data_folder_path):
     """
     Process all bag files in the given folder using multiprocessing
     """
-    bag_files = list(find_rosbag_files(raw_data_folder_path))  # Get the list of ROS bag files
-    cpu_count = psutil.cpu_count()  # Get the number of CPU cores
-    target_processes = int(cpu_count * 0.8)  # Calculate the target number of processes (80% of CPU cores)
-    pool = multiprocessing.Pool(processes=target_processes)  # Create a multiprocessing Pool with target number of processes
-    pool.map(process_bag_file, bag_files)  # Map the bag processing function to the pool
-    pool.close()  # Close the pool
-    pool.join()  # Wait for all processes in the pool to finish
-
+    bag_files = list(find_rosbag_files(raw_data_folder_path))
+    cpu_count = psutil.cpu_count()
+    target_processes = int(cpu_count * 0.8)
+    pool = multiprocessing.Pool(processes=target_processes)
+    pool.map(process_bag_file, bag_files)
+    pool.close()
+    pool.join()
 
 if __name__ == "__main__":
-    raw_data_folder_path = r"F:\Data Analysis Sets\2023_05_13\Test1_10_03_C-R"  # Set the path to the raw data folder
-    process_bag_files(raw_data_folder_path)  # Process bag files in the specified folder
-    print('Done converting')  # Print a message indicating the conversion is complete
+    raw_data_folder_path = r"G:\Data Analysis Sets\2023_05_13\Test1_10_03_C-R"
+    process_bag_files(raw_data_folder_path)
+    print('Done converting')
